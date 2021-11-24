a51bc6b9cb6ce7eee7f11f16354e89aae7594ab2
brueth <94363393+brueth@users.noreply.github.com>
2021-11-24 21:52:15 +0100

    Update winbuild-cross.sh

--- winbuild-cross.sh
+++ winbuild-cross.sh
@@ -61,7 +61,7 @@ compile() {
   #CXXFLAGS="$CFLAGS -std=c++2a -fconcepts -Wno-ignored-attributes" \
 
   CFLAGS="-O3 -march=${1} ${3} ${DFLAGS}" \
-  CXXFLAGS="$CFLAGS -std=c++20 ${DCXXFLAGS}"  \
+  CXXFLAGS="$CFLAGS -std=c++2a ${DCXXFLAGS}"  \
   ./configure ${CONFIGURE_ARGS}
   make -j $(nproc)
   strip -s cpuminer.exe
